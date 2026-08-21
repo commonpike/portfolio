688bbea29cea65a9f8eb2465e6fb8501b3fb3dfb
pike <pike@GreyMono.local>
2026-08-21 14:57:55 +0200

    feat: Add content

--- web/src/views/CvView.vue
+++ web/src/views/CvView.vue
@@ -4,16 +4,116 @@ import SiteIntro from '@/components/SiteIntro.vue'
 
 <template>
   <SiteIntro>
+
     <p class="lede">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-      laboris nisi ut aliquip ex ea commodo consequat.
+      <b>Pieter van Kemenade</b>, 1967-09-08 Nijmegen, NL<br>
+      <a href="https://pike.kw.nl" target="_blank">
+        https://pike.kw.nl
+      </a><br>
+      <a href="https://pike.kw.nl/portfolio/" target="_blank">
+        https://pike.kw.nl/portfolio/
+      </a><br>
+      <a href="https://www.linkedin.com/in/commonpike/" target="-blank">
+        https://www.linkedin.com/in/commonpike/
+      </a><br>
+
+      <br>
+      Selfemployed in the partnership <b>"kennis Werkt"</b>, Rotterdam, NL
+    </p>
+    <p class="muted">
+      I've studied Mathematics in Leiden, NL and Photography/Audio-Visual Design in Rotterdam, NL, which I finished in 1995. At the moment I am selfemployed within the partnership "kennis Werkt", Rotterdam, NL.
+      <br><br>
+      During the last 30 years I have been professionally involved in various projects involving internet, culture and data exchange, usually as a freelancer, often part of a team. Allthough educated as a designer (at the art academy in Rotterdam), my IT background was higher valued and I accumulated the practical knowledge and skills as time went by. Nowadays I am mostly known for a broad practical experience and wide applicability, and the capacity to learn quickly and adapt.
+      <br><br>
+      In the remaining time, if any, I am a graphic artist, musician and sound designer.
     </p>
 
-    <p class="muted">
-      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-      mollit anim id est laborum.
-    </p>
+    <h3>Areas of Knowledge</h3>
+    <ul>
+        <li><b>programming</b> 	( Node/Typescript/Javascript, PHP, Python, Java, XSLT, Actionscript, Perl )</li>
+      	<li><b>databases/cms</b> 	( MySQL, Drupal, Wordpress, ezPublish, FileMaker )</li>
+        <li><b>markuplanguages</b>	( Json, XML, RDF, OWL, XUL )</li>
+        <li><b>frameworks</b> 	( Vue, Symfony )</li>
+        <li><b>webdesign</b>		( HTML, CSS  )</li>
+    </ul>
+
+    <h3>Professional History</h3>
+    <ul>
+      <li><b>2016-2020:</b> 	  various assigments (ao an app for Forbo),
+        general maintenance and features for IAF World (https://iaf-world.org),
+        initial development of Mediakunst.net,
+        several projects for ProDemos (https://prodemos.nl)
+      </li>
+      <li><b>2012-2016:</b> 	  various assigments (ao https://sujata.nl),
+        several project for ProDemos, several annual reports for Liberty Global</li>
+      <li><b>2011:</b> 	  various assigments (ao http://panorama.intercultural-europe.org)</li>
+      <li><b>2010:</b> 	  various assigments (ao http://www.peacebuilding.no/, http://www.hivosklimaatfonds.nl/);
+          parttime freelancer at http://www.contactivity.com,
+          database management and API design for http://catalogue.nimk.nl</li>
+      <li><b>2006-2009:</b> various assigments (ao http://www.publicdomainarchitecten.nl);
+          technical manager / developer for http://www.labforculture.org </li>
+      <li><b>2005-2006:</b> various assigments (ao http://www.amateurkunst.net, 3D illustrations
+          for ITEA/PassePartout, XULApplication for the administration of SBAKunst)</li>
+      <li><b>2002-2004:</b> parttime employment at V2_ Organisation (http://www.v2.nl/)
+          as archive- and webdeveloper (ao http://archive.v2.nl/) </li>
+      <li><b>2000-2001:</b> parttime freelancer at V2_ Organisation (ao http://deaf.v2.nl/03/)
+          produced various websites (ao http://www.onzeboomgaard.nl)</li>
+      <li><b>2000-2002:</b> involved in the development of the archive and website of photoagency ABCPress</li>
+      <li><b>2000:</b>	  involved in the  development of video and CDRom productions through kennis Werkt</li>
+      <li><b>1998-1999:</b> parttime employment at Spirit Interactieve Diensten
+          as webdesigner (ao VVV Rotterdam and Rotterdam Business Park)
+          produced various websites (ao http://www.v2.nl/deaf/98/)</li>
+      <li><b>1996-1997:</b> various websites (ao Dutch Electronic Art Festival, Film Festival Rotterdam)</li>
+      <li><b>1995-1996:</b> development of Digitale Stad Rotterdam, various websites</li>
+      <li><b>1995:</b>	  engaged in the partnership "kennis Werkt" (Rotterdam)</li>
+      <li><b>1995: </b> 	  graduation assignment art academie: website Film Festival Rotterdam </li>
+      <li><b>1994: </b> 	  art academie: slideshow for the "Bedrijfsfonds voor de Pers"</li>
+      <li><b>1991-1992: </b> autonomously employed in the audiovisual sector</li>
+    </ul>
+
+    <h3>Areas of expertise</h3>
+	  <ul>
+      <li>Information architecture</li>
+	    <li>Interface Design, front- & backend </li>
+	    <li>Project development and -management</li>
+      <li>Database & systemmanagement and -maintainance</li>
+    </ul>
+
+    <h3>Autonomous Projects</h3>
+    <ul>
+        <li>
+          <b>2020-2024:</b> playing hammond in rockband On Solid Ground
+            (<a href="https://open.spotify.com/album/0HHSRv4rsnde0WAaYHyIkX?si=CrA8IdBkTIexpqW10fKTqA" target="_blank">Spotify</a>)
+        </li>
+        <li><b>2016-now:</b> coding graphic art and video as STDIO.PIKE: <a href="https://pike.kw.nl/stdio" target="_blank">https://pike.kw.nl/stdio</a></li>
+        <li>
+          <b>2012-2016:</b> playing hammondkeyboards and organ in rockband Weltmeister
+            (<a href="https://open.spotify.com/album/4nCXLM028qYLoo9bJ1BlQ3?si=cXN-_THOTYOOoAeleYD3Uw" target="_blank">Spotify</a>)
+        </li>
+        <li>
+          <b>2008-2010:</b> on tour (Holland, Canada,America) playing keyboard and computers with noise-rockband Venus Flytrap
+            (<a href="https://open.spotify.com/album/1gaZgBKtd28mv6D4SwqSL5?si=glRMWoLTQyW9mLNSuVTtzQ" target="_blank">Spotify</a>)
+        </li>
+	      <li><b>2001:</b> 	  audio design for a public space in A'dam ("Maris Darz") </li>
+        <li><b>1998:</b> 	  audio design for a room without entrance in Goes ("Stella Maris")</li>
+        <li><b>1992-1995:</b> involved as audiodesigner in the development of a
+                  automated environment ("Loka", st. Enzoch). Yearly exposition.</li>
+      </ul>
+
+    <h3>Educational History</h3>
+    <ul>
+      <li><b>1992 - 1995:</b> Photography/Audio-Visual Design artacademie Rotterdam</li>
+      <li><b>1989 - 1995:</b> Various courses audio engineering Muzisch Centrum TU Delft </li>
+      <li><b>1989 - 1991:</b> Graphic/Audio-Visual Design artacademie Rotterdam</li>
+      <li><b>1987 - 1989:</b> Bachelor of Mathematics / IT Leiden University</li>
+      <li><b>1979 - 1987:</b> HAVO 1t/m5 + VWO 5,6 Haags Montessori Lyceum</li>
+    </ul>
+
+    <h3>Prefered Development Software</h3>
+    <p>VSCode, BBEdit, Sketch, Affinity Designer, Photoshop, Ableton Live, bash :)</p>
+
+    <h3>Prefered Operating Systems</h3>
+    <p>Mac OS, Linux, Windows (in that order)</p>
+
   </SiteIntro>
 </template>
--- web/src/views/PortfolioView.vue
+++ web/src/views/PortfolioView.vue
@@ -6,15 +6,11 @@ import ProjectsListing from '@/components/ProjectsListing.vue'
 <template>
   <SiteIntro>
     <p class="lede">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-      laboris nisi ut aliquip ex ea commodo consequat.
-    </p>
-
-    <p class="muted">
-      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-      mollit anim id est laborum.
+      Keeping track of everything I did is hard.
+      Projects overlap, get revived, linger on, die off.
+      Once they're out of sight, I often forget to take a screenshot,
+      make some notes. After all, it's online, people can
+      look there - until it's gone. But now it's here, or most of it.
     </p>
   </SiteIntro>
 
